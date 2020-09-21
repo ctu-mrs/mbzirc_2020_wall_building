81368760536c1cb7bdcfac694ea48da2bb8cbb53
klaxalk <klaxalk@gmail.com>
2020-09-21 15:26:00 +0200

    updated travis

--- .ci/ci_before_install.sh
+++ .ci/ci_before_install.sh
@@ -8,13 +8,14 @@ distro=`lsb_release -r | awk '{ print $2 }'`
 
 echo "Starting install preparation"
 
-openssl aes-256-cbc -K $encrypted_f0fd3ee254e8_key -iv $encrypted_f0fd3ee254e8_iv -in ./.ci/deploy_key_github.enc -out ./.ci/deploy_key_github -d
-eval "$(ssh-agent -s)"
-chmod 600 ./.ci/deploy_key_github
-ssh-add ./.ci/deploy_key_github
-
 sudo apt-get update -qq
 sudo apt-mark hold openssh-server
+
+# the "gce-compute-image-packages" package often freezes the installation at some point
+# the installation freezes when it tries to manage some systemd services
+# this attempts to install the package and stop the problematic service during the process
+((sleep 90 && (sudo systemctl stop google-instance-setup.service && echo "gce service stoped" || echo "gce service not stoped")) & (sudo timeout 120s apt-get -y install gce-compute-image-packages)) || echo "\e[1;31mInstallation of gce-compute-image-packages failed\e[0m"
+
 sudo apt -y upgrade --fix-missing
 sudo apt-get install dpkg git python-setuptools python3-setuptools python3-pip
 
